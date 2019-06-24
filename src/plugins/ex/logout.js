2965de3eafde474f4693343b47c126526579e0ed
Kananek Thongkam <info.dvgamer@gmail.com>
2019-06-24 16:46:40 +0700

    remove webpack.

--- src/plugins/ex/logout.js
+++
@@ -1,9 +0,0 @@
-import request from '../lib/request'
-
-export default async (step03) => {
-  if (!global._appToken.step03) return
-
-  console.log(`[FORUMS] logout-get: ${global._appToken.step03}`)
-  let html = await request('GET', global._appToken.step03)
-  return /redirectwrap[\W\w]*?<h4>Thanks/ig.test(html)
-}
--- src/plugins/lib/parse-image.js
+++
@@ -1,4 +0,0 @@
-export default html => {
-  let gdtm = html.match(/gdtm".*?<a href=".*?">/ig) || []
-  return gdtm.map(item => /href="(?<link>.*?)"/ig.exec(item).groups.link)
-}

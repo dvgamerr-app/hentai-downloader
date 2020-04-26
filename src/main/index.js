8f977a98d4bf6353cc9c46b6ba83de9448807540
Kananek Thongkam <info.dvgamer@gmail.com>
2020-04-26 20:16:13 +0700

    Change core project.

--- src/main/index.js
+++ src/main/index.js
@@ -1,115 +1,20 @@
-'use strict'
-import { app, BrowserWindow } from 'electron'
-import path from 'path'
-
-import * as vEvents from '../plugins/events'
+/* globals INCLUDE_RESOURCES_PATH */
+import { app } from 'electron'
 
 /**
- * Set `__static` path to static files in production
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ * Set `__resources` path to resources files in renderer process
  */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = path.join(__dirname, '/static').replace(/\\/g, '\\\\')
-}
+global.__resources = undefined // eslint-disable-line no-underscore-dangle
+// noinspection BadExpressionStatementJS
+INCLUDE_RESOURCES_PATH // eslint-disable-line no-unused-expressions
+if (__resources === undefined) console.error('[Main-process]: Resources path is undefined')
 
-// let appIcon
-let mainWindow
-let mainConfig
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
-
-function createWindow () {
-  /**
-   * Initial window options
-   */
-
-  mainConfig = {
-    width: 600,
-    height: 360,
-    minWidth: 600,
-    minHeight: 360,
-    maxWidth: 600,
-    maxHeight: 360,
-    'node-integration': false,
-    title: app.getName(),
-    icon: path.join(__dirname, 'src/renderer/assets/touno.ico'),
-    show: true,
-    // frame: false,
-    movable: true,
-    resizable: false,
-    alwaysOnTop: false,
-    skipTaskbar: false,
-    transparent: false
-  }
-
-  // const padding = 10
-  // var screenSize = screen.getPrimaryDisplay().workAreaSize
-  // mainConfig.x = screenSize.width - mainConfig.width - padding
-  // mainConfig.y = screenSize.height - mainConfig.height - padding
-
-  // appIcon = new Tray(path.join(__dirname, '../renderer/assets/stats/icon-offline.png'))
-  // var contextMenu = Menu.buildFromTemplate([
-  //   {
-  //     label: 'Close',
-  //     click: () => {
-  //       mainWindow.close()
-  //     }
-  //   }
-  // ])
-
-  // appIcon.setToolTip(mainConfig.title)
-  // appIcon.setContextMenu(contextMenu)
-
-  // appIcon.on('click', (e, bounds) => {
-  //   mainWindow.show()
-  // })
-
-  mainWindow = new BrowserWindow(mainConfig)
-  mainWindow.loadURL(winURL)
-  mainWindow.setMenu(null)
-
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-
-  mainWindow.on('blur', () => {
-    // mainWindow.hide()
-  })
-
-  vEvents.server(mainWindow)
-}
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
-
-/*
-import { autoUpdater } from 'electron-updater'
-
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
-
-app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-})
- */
+// Load here all startup windows
+require('./mainWindow')
